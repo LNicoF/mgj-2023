21fbd7fb9715cc7984b485034b6d572609681736
Nacho <elnachoed@gmail.com>
2023-10-07 17:37:50 -0500

    animacion player
    va

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -6,11 +6,11 @@ var speed = 5000
 var velocity := Vector2()
 
 func _physics_process( delta ):
-    velocity = _move() * delta
-    velocity = move_and_slide( velocity )
+	velocity = _move() * delta
+	velocity = move_and_slide( velocity )
 
 func _move() -> Vector2 :
-    var dist = _player.position - position
-    var dir = Vector2( sign( dist.x ), sign( dist.y ) )
-    dir = dir.normalized()
-    return Isometric.calcVec( dir.normalized() * speed )
+	var dist = _player.position - position
+	var dir = Vector2( sign( dist.x ), sign( dist.y ) )
+	dir = dir.normalized()
+	return Isometric.calcVec( dir.normalized() * speed )
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,23 +6,23 @@ var velocity := Vector2()
 onready var _animation_player = $AnimationPlayer
 
 func _physics_process(delta):
-    velocity = _move() * delta
-    velocity = move_and_slide( velocity )
-    _animate()
+	velocity = _move() * delta
+	velocity = move_and_slide( velocity )
+	_animate()
 
 func _move() -> Vector2 :
-    var goRight := Input.is_physical_key_pressed( KEY_D )
-    var goLeft  := Input.is_physical_key_pressed( KEY_A )
-    var goUp    := Input.is_physical_key_pressed( KEY_W )
-    var goDown  := Input.is_physical_key_pressed( KEY_S )
-    var xDir := int( goRight ) - int( goLeft )
-    var yDir := int( goDown )  - int( goUp )
-    var res = Vector2( xDir, yDir ).normalized() * SPEED
-    return Isometric.calcVec( res )
+	var goRight := Input.is_physical_key_pressed( KEY_D )
+	var goLeft  := Input.is_physical_key_pressed( KEY_A )
+	var goUp    := Input.is_physical_key_pressed( KEY_W )
+	var goDown  := Input.is_physical_key_pressed( KEY_S )
+	var xDir := int( goRight ) - int( goLeft )
+	var yDir := int( goDown )  - int( goUp )
+	var res = Vector2( xDir, yDir ).normalized() * SPEED
+	return Isometric.calcVec( res )
 
 func _animate() :
-    if velocity == Vector2() :
-        _animation_player.stopMoving()
-    else :
-        _animation_player.startMoving()
+	if velocity == Vector2() :
+		_animation_player.stopMoving()
+	else :
+		_animation_player.startMoving()
 
