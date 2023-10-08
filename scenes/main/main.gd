a8d02b167b7187e8da5c8d5523cf51d9a4a6fb70
Nacho <elnachoed@gmail.com>
2023-10-07 23:30:41 -0500

    animaciones recuperadas
    This reverts commit 856eb4308bcd4b75b8dd9637d2bf635af88898f5, reversing
    changes made to aed287836dfe1029209686cf1e722db7b5f43059.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -9,21 +9,21 @@ onready var _enemyPrototype := $YSort/Enemy ;
 onready var _player := $YSort/Player
 
 func _spawn( rPosition: Vector2 ) -> void :
-    if entitiesContainer.get_child_count() > entityCap:
-        return
-    var enemy = _createEnemy( rPosition )
-    entitiesContainer.add_child( enemy )
+	if entitiesContainer.get_child_count() > entityCap:
+		return
+	var enemy = _createEnemy( rPosition )
+	entitiesContainer.add_child( enemy )
 
 func _on_Spawner_timeout( position: Vector2 ):
-    _spawn( position )
+	_spawn( position )
 
 func _createEnemy( rPosition: Vector2 ) -> Node :
-    var enemy : Node2D = Enemy.instance()
-    enemy.position = rPosition + _player.position
-    print( 'player at %s' % _player.position )
-    print( 'spawning at %d' % pitagoras( rPosition ) )
-    enemy.scale = _enemyPrototype.scale
-    return enemy
+	var enemy : Node2D = Enemy.instance()
+	enemy.position = rPosition + _player.position
+	print( 'player at %s' % _player.position )
+	print( 'spawning at %d' % pitagoras( rPosition ) )
+	enemy.scale = _enemyPrototype.scale
+	return enemy
 
 func pitagoras( oa: Vector2 ) -> float:
-    return sqrt( oa.x * oa.x + oa.y * oa.y ) ;
+	return sqrt( oa.x * oa.x + oa.y * oa.y ) ;
